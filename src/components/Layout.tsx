824108cadc574694cebd08d8cbf3af4bc8e3d01f
Tupchu <tupchu5@gmail.com>
2025-04-04 19:09:40 -0500

    Added footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,10 @@
 import Sidebar from "./Sidebar";
 import MainContent from "./MainContent";
+import Footer from "../sections/Footer";
 
 const Layout = () => {
   return (
-    <div className="min-h-screen bg-gray-100 text-black py-8">
+    <div className="min-h-screen bg-gray-100 text-black pt-8">
       {/* Container centers the layout and adds gutter on both sides */}
       <div className="max-w-6xl mx-auto flex flex-col lg:flex-row gap-6 px-4 lg:px-0">
         {/* Sidebar */}
@@ -16,6 +17,7 @@ const Layout = () => {
           <MainContent />
         </main>
       </div>
+      <Footer />
     </div>
   );
 };
---
+++ src/sections/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="flex border-t-1 justify-center items-center py-6 border-gray-300 mt-12">
+      <p className="text-gray-700 leading-relaxed text-base">
+        Timothy Upchurch
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
