1fd70bebbc3e20405c825da6d76416bc17d789dd
Tupchu <tupchu5@gmail.com>
2025-04-04 21:46:52 -0500

    Added contact link

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -69,9 +69,11 @@ const Sidebar = () => {
               <button className="w-full border border-gray-300 py-2 rounded-md cursor-pointer hover:bg-gray-100 transition-all duration-300">
                 Download CV
               </button>
-              <button className="w-full bg-black text-white py-2 rounded-md cursor-pointer hover:bg-black/80 transition-all duration-300">
-                <a href="#contact">Contact Me</a>
-              </button>
+              <a href="#contact">
+                <button className="w-full bg-black text-white py-2 rounded-md cursor-pointer hover:bg-black/80 transition-all duration-300">
+                  Contact Me
+                </button>
+              </a>
             </div>
           </div>
         </motion.div>
