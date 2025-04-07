b11f76462fb2a0de2165b222eeb1a713d0638c84
Tupchu <tupchu5@gmail.com>
2025-04-06 20:27:16 -0500

    Added active states for theme toggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -27,7 +27,7 @@ export default function ThemeToggle() {
   return (
     <button
       onClick={toggleTheme}
-      className="px-3 py-2 rounded-md bg-gray-100 border border-gray-300 hover:bg-gray-300 text-sm text-gray-800 dark:bg-gray-700 dark:border-gray-700 dark:hover:bg-gray-600 dark:hover:border-gray-600 dark:text-gray-100 transition-colors "
+      className="px-3 py-2 rounded-md bg-gray-100 border border-gray-300 hover:bg-gray-300 active:bg-gray-100 text-sm text-gray-800 dark:bg-gray-700 dark:border-gray-700 dark:hover:bg-gray-600 dark:hover:border-gray-600 dark:active:bg-gray-700 dark:active:border-gray-700 dark:text-gray-100 transition-colors "
     >
       {theme === "dark" ? (
         <span className="flex gap-2 justify-center">
